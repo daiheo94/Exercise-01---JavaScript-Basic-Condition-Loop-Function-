515c664c7218e4eb930be8ce13e014dcfcf3742e
Huỳnh Đức Đông Thi <84487938+daiheo94@users.noreply.github.com>
2021-06-04 17:05:14 +0700

    Add files via upload

---
+++ kiemtrasohoanhaotrongmang.js
@@ -0,0 +1,20 @@
+var myArray = [
+    2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
+];  
+        var i=0;
+    for(arr.myArray[i]; i = myArray.length; i++){
+        var tong=0;
+        
+        arr.myArray[i] = Number(n);
+    for (var a =1; i<=arr.myArray[i] ; a++){
+    if (arr.myArray[i] % a == 0){
+        tong=tong+a;
+    }
+}
+    if (tong == arr.myArray[i]){
+    console.log( arr.myArray[i] + 'la so hoan hao')
+}
+    else{
+    console.log( arr.myArray[i] + ' k la so hoan hao')
+}
+    }
